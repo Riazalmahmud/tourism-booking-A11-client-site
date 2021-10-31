8d0bcacbedad2db906f467222e8ea4e043aea758
Riazalmahmud <riazalmahmud041@gmail.com>
2021-10-31 23:57:50 +0600

    added

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -32,8 +32,9 @@ const Header = () => {
                                 <Button onClick={logOut} variant="light">logOut</Button> :
                                 <Nav.Link as={Link} to="/login">login</Nav.Link>
                             }
-                            <NavDropdown id="collasible-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1"><img className="placeOrder-photo" src={user.photoURL} alt="" className="bg-dark" /></NavDropdown.Item>
+                            <NavDropdown id="collasible-nav-dropdown" className="primar-custom-color" >
+                                {user?.email ? <li className="text-white fs-6"> <img className="placeOrder-photo" src={user.photoURL} alt="" /> <br /> {user.displayName} <br /> {user.email} </li> : <NavDropdown.Item href="#action/3.1"></NavDropdown.Item>}
+
                                 <Nav.Link as={Link} to="/home">Home</Nav.Link>
                                 <Nav.Link as={Link} to="/ManageAllOrders">Manage All Booking</Nav.Link>
                                 <Nav.Link as={Link} to="/PlaceOrder">PlaceOrder</Nav.Link>
